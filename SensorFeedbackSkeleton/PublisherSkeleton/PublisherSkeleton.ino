982d5455cf64169ac37aa1b6777d892937a0c933
Michael Milic <mikemilic98@gmail.com>
2019-12-27 13:26:43 -0800

    skeleton files for gyro feedback and pub sub outline

---
+++ SensorFeedbackSkeleton/PublisherSkeleton/PublisherSkeleton.ino
@@ -0,0 +1,116 @@
+/*
+ * rosserial PubSub Example
+ * Prints "hello world!" and toggles led
+ */
+
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+
+ros::NodeHandle  nh;
+
+
+void messageCb( const std_msgs::Empty& toggle_msg){
+  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
+}
+
+ros::Subscriber<std_msgs::Empty> sub("toggle_led", messageCb );
+
+
+
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+char hello[13] = "hello world!";
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  nh.initNode();
+  nh.advertise(chatter);
+  nh.subscribe(sub);
+}
+
+void loop()
+{
+  str_msg.data = hello;
+  chatter.publish( &str_msg );
+  nh.spinOnce();
+  delay(500);
+}
+
+
+/*
+ * rosserial Temperature Sensor Example
+ * 
+ * This tutorial demonstrates the usage of the
+ * Sparkfun TMP102 Digital Temperature Breakout board
+ * http://www.sparkfun.com/products/9418
+ * 
+ * Source Code Based off of:
+ * http://wiring.org.co/learning/libraries/tmp102sparkfun.html
+
+
+#include <Wire.h>
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
+ros::NodeHandle  nh;
+
+
+std_msgs::Float32 temp_msg;
+ros::Publisher pub_temp("temperature", &temp_msg);
+
+
+// From the datasheet the BMP module address LSB distinguishes
+// between read (1) and write (0) operations, corresponding to 
+// address 0x91 (read) and 0x90 (write).
+// shift the address 1 bit right (0x91 or 0x90), the Wire library only needs the 7
+// most significant bits for the address 0x91 >> 1 = 0x48
+// 0x90 >> 1 = 0x48 (72)
+
+int sensorAddress = 0x91 >> 1;  // From datasheet sensor address is 0x91
+                                // shift the address 1 bit right, the Wire library only needs the 7
+                                // most significant bits for the address
+
+
+void setup()
+{
+  Wire.begin();        // join i2c bus (address optional for master) 
+  
+  nh.initNode();
+  nh.advertise(pub_temp);
+  
+}
+
+long publisher_timer;
+
+void loop()
+{
+  
+  if (millis() > publisher_timer) {
+  // step 1: request reading from sensor 
+    Wire.requestFrom(sensorAddress,2); 
+    delay(10);
+    if (2 <= Wire.available())  // if two bytes were received 
+    {
+      byte msb;
+      byte lsb;
+      int temperature;
+      
+      msb = Wire.read();  // receive high byte (full degrees)
+      lsb = Wire.read();  // receive low byte (fraction degrees) 
+      temperature = ((msb) << 4);  // MSB
+      temperature |= (lsb >> 4);   // LSB
+
+      temp_msg.data = temperature*0.0625;
+      pub_temp.publish(&temp_msg);
+    }
+  
+  publisher_timer = millis() + 1000;
+  }
+  
+  nh.spinOnce();
+}
+
+*/
