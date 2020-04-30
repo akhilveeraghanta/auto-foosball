573888036a2a58be1067221b561f4861a6472e0c
akhil <akhil.veeraghanta@outlook.com>
2020-04-30 12:14:54 -0700

    finish integration, needs tweatking

--- src/vision/detections/ball_detection/track_foos_ball.py
+++ src/vision/detections/ball_detection/track_foos_ball.py
@@ -1,48 +1,69 @@
 #!/usr/bin/env python
 
 # import the necessary packages
-from __future__ import print_function
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 import rospy
 import sys
 from collections import deque
-from imutils.video import VideoStream
 import numpy as np
 import argparse
 import cv2
 
 import imutils
 import time
-import roslib
-roslib.load_manifest('my_package')
 
 #######################################################################
-#                               GLOBAL                                #
+#                               Globals                               #
 #######################################################################
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=32, help="max buffer size")
+args = vars(ap.parse_args())
 
 # NOTE TO SELF: change the color here to white or whatever
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space
-WHITE_LOWER = (0, 0, 250)
-WHITE_UPPER = (255, 255, 255)
+whiteLower = (0, 0, 250)
+whiteUpper = (255, 255, 255)
 
 # initialize the list of tracked points, the frame counter,
 # and the coordinate deltas
-POINTS = deque(maxlen=args["buffer"])
-COUNTER = 0
+pts = deque(maxlen=args["buffer"])
+counter = 0
 (dX, dY) = (0, 0)
-DIRECTION = ""
+direction = ""
 
-# keep looping
-while True:
+# TODO cleanup, globals aren't ideal
+image_pub = None
+image_sub = None
+bridge = None
+
+#######################################################################
+#                               Handler                               #
+#######################################################################
+
+
+def handle_new_frame(data):
+    """Handle a new frame and detect the soccer ball
+
+    :param data: The data from the image callback
+    :type data: sensor_msgs.Image
+    :returns: None
+    :rtype: None
+
+    """
+    global direction, dX, dY
+    frame = bridge.imgmsg_to_cv2(data, "bgr8")
+
     # resize the frame, blur it, and convert it to the HSV
     # color space
     frame = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
 
     # NOSTE TO SELF: Change the Mask Here to White for a foosball
     # construct a mask for the color "green", then perform
@@ -78,121 +99,68 @@ while True:
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
             pts.appendleft(center)
 
-    # loop over the set of tracked points
-    for i in np.arange(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
-        if pts[i - 1] is None or pts[i] is None:
-            continue
+        # loop over the set of tracked points
+        for i in np.arange(1, len(pts)):
+            # if either of the tracked points are None, ignore
+            # them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
 
-        # check to see if enough points have been accumulated in
-        # the buffer
-        if counter >= 10 and i == 1 and pts[-10] is not None:
-            # compute the difference between the x and y
-            # coordinates and re-initialize the direction
-            # text variables
-            dX = pts[-10][0] - pts[i][0]
-            dY = pts[-10][1] - pts[i][1]
-            (dirX, dirY) = ("", "")
+            # check to see if enough points have been accumulated in
+            # the buffer
+            if counter >= 10 and i == 1 and pts[-10] is not None:
+                # compute the difference between the x and y
+                # coordinates and re-initialize the direction
+                # text variables
+                dX = pts[-10][0] - pts[i][0]
+                dY = pts[-10][1] - pts[i][1]
+                (dirX, dirY) = ("", "")
 
-            # ensure there is significant movement in the
-            # x-direction
-            if np.abs(dX) > 20:
-                dirX = "East" if np.sign(dX) == 1 else "West"
+                # ensure there is significant movement in the
+                # x-direction
+                if np.abs(dX) > 20:
+                    dirX = "East" if np.sign(dX) == 1 else "West"
 
-            # ensure there is significant movement in the
-            # y-direction
-            if np.abs(dY) > 20:
-                dirY = "North" if np.sign(dY) == 1 else "South"
+                # ensure there is significant movement in the
+                # y-direction
+                if np.abs(dY) > 20:
+                    dirY = "North" if np.sign(dY) == 1 else "South"
 
-            # handle when both directions are non-empty
-            if dirX != "" and dirY != "":
-                direction = "{}-{}".format(dirY, dirX)
+                # handle when both directions are non-empty
+                if dirX != "" and dirY != "":
+                    direction = "{}-{}".format(dirY, dirX)
 
-            # otherwise, only one direction is non-empty
-            else:
-                direction = dirX if dirX != "" else dirY
+                # otherwise, only one direction is non-empty
+                else:
+                    direction = dirX if dirX != "" else dirY
 
-                # otherwise, compute the thickness of the line and
-            # draw the connecting lines
-            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+                    # otherwise, compute the thickness of the line and
+                # draw the connecting lines
+                thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+                cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-    # show the movement deltas and the direction of movement on
-    # the frame
-    cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-                0.65, (0, 0, 255), 3)
-    cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                0.35, (0, 0, 255), 1)
+        # show the movement deltas and the direction of movement on
+        # the frame
+        cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65, (0, 0, 255), 3)
+        cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
+                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.35, (0, 0, 255), 1)
 
-    # show the frame to our screen and increment the frame counter
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    counter += 1
+        # show the frame to our screen and increment the frame counter
+    image_pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
 
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        break
-
-# if we are not using a video file, stop the camera video stream
-if not args.get("video", False):
-    vs.stop()
-
-
-# otherwise, release the camera
-else:
-    vs.release()
-
-# close all windows
-cv2.destroyAllWindows()
-
-
-class ClassAdapter(object):
-
-    """Converts the incoming frames to opencv"""
-
-    def __init__(self):
-        """TODO: to be defined1. """
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("image_topic", Image, self.callback)
-
-
-class CameraAdapter:
-
-    def __init__(self):
-
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("image_topic", Image, self.callback)
-
-    def callback(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
-
-        (rows, cols, channels) = cv_image.shape
-        if cols > 60 and rows > 60:
-            cv2.circle(cv_image, (50, 50), 10, 255)
-
-        cv2.imshow("Image window", cv_image)
-        cv2.waitKey(3)
-
-        try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-        except CvBridgeError as e:
-            print(e)
-
-
-def main(args):
-    ic = image_converter()
-    rospy.init_node('image_converter', anonymous=True)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
-    cv2.destroyAllWindows()
+#######################################################################
+#                                Main                                 #
+#######################################################################
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+
+    rospy.init_node('image_converter', anonymous=True)
+
+    bridge = CvBridge()
+    image_sub = rospy.Subscriber("/camera", Image, handle_new_frame)
+    image_pub = rospy.Publisher("/camera/ball_detection", Image, queue_size=1)
+
+    rospy.spin()
